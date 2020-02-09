05ac65626553438cb5622109633e1ccf7680ee58
Arun Kumar Tripathi <arun9483@gmail.com>
2020-02-09 11:15:30 +0530

    support avtar upload

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 const axios = require('axios');
-
-const router = express.Router();
+const multer = require('multer');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname));
+
+app.use(cors());
+const router = express.Router();
 
 async function getUsers() {
   const resp = await axios.get('https://randomuser.me/api/?results=5');
@@ -22,8 +26,47 @@ router.get('/users', (req, res) => {
     });
 });
 
-app.use(cors());
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    const uploadDir = path.join(__dirname, '/uploads');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, path.join(__dirname, '/uploads/'));
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    // rejects storing a file
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+
+router.route('/upload-avtar').post(upload.single('avtar'), (req, res) => {
+  const imageData = req.file.path;
+  console.log('imageData', imageData);
+  res.status(200).json({
+    success: true
+  });
+});
+
 app.use('/', router);
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`express server started at port ${PORT}`);
 });
